7600cfe700a29812192f589c8c6cc8090a72b5bf
alexis51151 <44121643+alexis51151@users.noreply.github.com>
2020-05-28 10:51:13 +0200

    Modification des CMakeLists pour box2D

---
+++ out/build/x64-Debug/unitTests/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/alexi/source/repos/Jeu_jin/unitTests
+# Build directory: C:/Users/alexi/source/repos/Jeu_jin/out/build/x64-Debug/unitTests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(that-test-I-made "C:/Users/alexi/source/repos/Jeu_jin/out/build/x64-Debug/unitTests/unitTests.exe")
+set_tests_properties(that-test-I-made PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/alexi/source/repos/Jeu_jin/unitTests/CMakeLists.txt;43;add_test;C:/Users/alexi/source/repos/Jeu_jin/unitTests/CMakeLists.txt;0;")
+subdirs("googletest-build")
---
+++ out/build/x64-Debug/unitTests/googletest-build/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/alexi/source/repos/Jeu_jin/out/build/x64-Debug/unitTests/googletest-src/googlemock
+# Build directory: C:/Users/alexi/source/repos/Jeu_jin/out/build/x64-Debug/unitTests/googletest-build/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
