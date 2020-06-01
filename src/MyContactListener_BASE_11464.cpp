8ffa325350d7968b2b36036194f000432b084c79
Bastien Porte <bastien.porte@telecom-sudparis.eu>
2020-06-01 19:46:12 +0200

    hotfixx

---
+++ src/MyContactListener_BASE_11464.cpp
@@ -0,0 +1,23 @@
+#include "MyContactListener.h"
+
+void MyContactListener::BeginContact(b2Contact* contact) {
+    //check if fixture A was the foot sensor
+    void* fixtureUserData = contact->GetFixtureA()->GetUserData();
+    if ((int)fixtureUserData == 3)
+        numFootContact++;
+    //check if fixture B was the foot sensor
+    fixtureUserData = contact->GetFixtureB()->GetUserData();
+    if ((int)fixtureUserData == 3)
+        numFootContact++;
+}
+
+void MyContactListener::EndContact(b2Contact* contact) {
+    //check if fixture A was the foot sensor
+    void* fixtureUserData = contact->GetFixtureA()->GetUserData();
+    if ((int)fixtureUserData == 3)
+        numFootContact--;
+    //check if fixture B was the foot sensor
+    fixtureUserData = contact->GetFixtureB()->GetUserData();
+    if ((int)fixtureUserData == 3)
+        numFootContact--;
+}
