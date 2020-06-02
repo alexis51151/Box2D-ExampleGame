186a48ee3d757422de8aed5d82a06ae638bc5809
alexis51151 <44121643+alexis51151@users.noreply.github.com>
2020-06-02 10:48:49 +0200

    Modifs minimes

--- src/Box2DEngine.cpp
+++ src/Box2DEngine.cpp
@@ -93,11 +93,11 @@ b2Body* Box2DEngine::addBodyPlayer(int x, int y, float height, float width) {
 	
 	b2PolygonShape npolygonShape;
 	//add foot sensor fixture
-	npolygonShape.SetAsBox(1, 1, b2Vec2(0, height*UNRATIO), 0);
-	b2FixtureDef mynFixtureDef;
-	mynFixtureDef.isSensor = true;
-	mynFixtureDef.shape = &npolygonShape;
-	b2Fixture* footSensorFixture = m_body->CreateFixture(&mynFixtureDef);
+	npolygonShape.SetAsBox(4, 4, b2Vec2(0, height*UNRATIO), 0);
+	myFixtureDef.isSensor = true;
+	myFixtureDef.shape = &npolygonShape;
+	myFixtureDef.density = 0;
+	b2Fixture* footSensorFixture = m_body->CreateFixture(&myFixtureDef);
 	
 	footSensorFixture->SetUserData((void*)3);
 	return m_body;
