365cffa0231f0e9511ddd7879995f0394c93575c
alexis51151 <44121643+alexis51151@users.noreply.github.com>
2020-06-03 21:10:41 +0200

    Modification de draw pour afficher en fonction du dataType et de la couleur en argument.

--- src/Draw.cpp
+++ src/Draw.cpp
@@ -4,7 +4,7 @@
 
 // Function for drawing a shape using SFML 
 // Only implements box2D fixtures that are rectangles (ie fixtures initialized using b2Fixture::setAsBox)
-void DrawShape(b2Body* body, sf::Shape* shape, sf::RenderWindow* window) {
+void DrawShape(b2Body* body, sf::Color color, sf::RenderWindow* window) {
 	float width = 0;
 	float height = 0;
 	b2Fixture* fixture = body->GetFixtureList();
@@ -27,6 +27,36 @@ void DrawShape(b2Body* body, sf::Shape* shape, sf::RenderWindow* window) {
 		return;
 	}
 	b2Vec2 position = body->GetPosition();
-	(*shape).setPosition((position.x - width / 2) * RATIO, (position.y - height / 2) * RATIO);
+	void* fixtureUserData = fixture->GetUserData();
+	FixtureData* fixtureData = (FixtureData*)fixtureUserData;
+
+	if (fixtureData == NULL) {
+		perror("No fixture data linked to that body\n");
+		return;
+	}
+	int dataType = (int)fixtureData->getDataType();
+	sf::Shape* shape;
+	switch (dataType) {
+	case (player || default):
+	{
+		sf::CircleShape c(width * RATIO);
+		c.setFillColor(color);
+		shape = &c;
+	}
+	case platform:
+	{
+		sf::RectangleShape r(sf::Vector2f(8.0f * 2 * RATIO, 1.0f * 2 * RATIO)); // *2 because box2D takes mid-height and mid-width 
+		r.setFillColor(sf::Color::Red);
+		shape = &r;
+	}
+	default:
+	{
+		perror("Not drawable dataType\n");
+		return;
+	}
+	}
+
+	// Setting position and drawing with SFML
+	shape->setPosition((position.x - width / 2) * RATIO, (position.y - height / 2) * RATIO);
 	window->draw(*shape);
 }
