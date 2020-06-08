40d7d7934f933cb07c0425b1a25cb39a46195bbf
Bastien Porte <bastien.porte@telecom-sudparis.eu>
2020-06-08 09:35:19 +0200

    utilisation des unique ptr dans monster [mabe note working]

--- src/Monster.cpp
+++ src/Monster.cpp
@@ -5,7 +5,7 @@ Monster::Monster()
 {
 }
 
-b2Body* addBodyMonster(Box2DEngine* gameController, int x, int y, float height, float whidth) {
+b2Body* Monster::addBodyMonster(Box2DEngine* gameController, int x, int y, float height, float whidth) {
 	b2BodyDef myBodyDef;
 	myBodyDef.type = b2_dynamicBody;
 	myBodyDef.fixedRotation = true;
@@ -28,8 +28,8 @@ b2Body* addBodyMonster(Box2DEngine* gameController, int x, int y, float height, 
 
 	//add main fixture
 	b2Fixture* playerFixture = m_body->CreateFixture(&myFixtureDef);
-	FixtureData* dataPlayer = new FixtureData(sf::Color::Green, Monstertype);
-	playerFixture->SetUserData(static_cast<void*>(dataPlayer));
+	Monster::my_maindata = std::make_unique < FixtureData>(sf::Color::Green, Monstertype);
+	playerFixture->SetUserData(static_cast<void*>(my_maindata.get()));
 
 	//add foot sensor fixture
 	b2PolygonShape LfootpolygonShape;
@@ -43,8 +43,8 @@ b2Body* addBodyMonster(Box2DEngine* gameController, int x, int y, float height, 
 	LfootFixtureDef.filter.groupIndex = MonsterLfoot;
 
 	b2Fixture* LfootSensorFixture = m_body->CreateFixture(&LfootFixtureDef);
-	FootData* dataLFoot = new FootData(sf::Color::Green, MonsterLfoot);
-	LfootSensorFixture->SetUserData(static_cast<void*>(dataLFoot));
+	Monster::my_Lfootdata = std::make_unique< FootData>(sf::Color::Green, MonsterLfoot);
+	LfootSensorFixture->SetUserData(static_cast<void*>(my_Lfootdata.get()));
 
 	//add foot sensor fixture
 	b2PolygonShape RfootpolygonShape;
@@ -58,8 +58,8 @@ b2Body* addBodyMonster(Box2DEngine* gameController, int x, int y, float height, 
 	RfootFixtureDef.filter.groupIndex = MonsterRfoot;
 
 	b2Fixture* RfootSensorFixture = m_body->CreateFixture(&RfootFixtureDef);
-	FootData* dataRFoot = new FootData(sf::Color::Green, MonsterRfoot);
-	RfootSensorFixture->SetUserData(static_cast<void*>(dataRFoot));
+	Monster::my_Rfootdata = std::make_unique< FootData>(sf::Color::Green, MonsterRfoot);
+	RfootSensorFixture->SetUserData(static_cast<void*>(my_Rfootdata.get()));
 
 	//add triangular sensor for the player 
 	const float radius = 8;
@@ -79,9 +79,8 @@ b2Body* addBodyMonster(Box2DEngine* gameController, int x, int y, float height, 
 	conefixtures.isSensor = true;
 	conefixtures.shape = &coneshape;
 	b2Fixture* conseSensorFixture = m_body->CreateFixture(&conefixtures);
-	ViewFieldData* consefixturedata = new ViewFieldData(sf::Color::Green,viewField);
-	conseSensorFixture->SetUserData(static_cast<void*>(consefixturedata));
-
+	Monster::my_Rviewdata = std::make_unique<ViewFieldData>(sf::Color::Green, viewField);
+	conseSensorFixture->SetUserData(static_cast<void*>(my_Rviewdata.get()));
 
 	return m_body;
 }
@@ -92,33 +91,11 @@ Monster::Monster(Box2DEngine* gameController, int x, int y, float height, float 
 	shape = std::unique_ptr<Rectangle>(new Rectangle());
 }
 
-FootData* Monster::getLeftfootData() {
-	b2Fixture* fixture = body->GetFixtureList();
-	while (fixture != nullptr) {
-		FixtureData* userdata = static_cast<FixtureData*>( fixture->GetUserData() );
-		if (userdata->getDataType()== MonsterLfoot) {
-			return (FootData*)userdata;
-		}
-		fixture = fixture->GetNext();
-	}
-	exit(1);
-}
-FootData* Monster::getRightfootData() {
-	b2Fixture* fixture = body->GetFixtureList();
-	while (fixture != nullptr) {
-		FixtureData* userdata = static_cast<FixtureData*>(fixture->GetUserData());
-		if (userdata->getDataType() == MonsterRfoot) {
-			return (FootData*)userdata;
-		}
-		fixture = fixture->GetNext();
-	}
-	exit(1);
-}
 
 void Monster::updatespeed()
 {
-	int lfootcontact = this->getLeftfootData()->GetNumFootContact();
-	int rfootcontact = this->getRightfootData()->GetNumFootContact();
+	int lfootcontact = this->my_Lfootdata->GetNumFootContact();
+	int rfootcontact = this->my_Rfootdata->GetNumFootContact();
 
 	if (rfootcontact > 1 && lfootcontact > 1) { //deux pied aux sol 
 		body->SetLinearVelocity(b2Vec2(this->directionxsigne()*10, 0));
