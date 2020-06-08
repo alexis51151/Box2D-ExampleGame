ca3e892c2b5dc6ef4f406fa5984c1dd2b4677f82
alexis51151 <44121643+alexis51151@users.noreply.github.com>
2020-06-08 16:34:45 +0200

    test interaction joueur-plateforme non fonctionnel

--- src/Player.cpp
+++ src/Player.cpp
@@ -86,11 +86,12 @@ b2Body* Player::addBodyPlayer(Box2DEngine* gameController, int x, int y, float h
 	return m_body;
 }
 
-Player::Player(Box2DEngine* gameController)
-{
-	Player::body = addBodyPlayer(gameController,10 * RATIO, 10 * RATIO, 1.0f * RATIO, 1.0f * RATIO);
+
+Player::Player(Box2DEngine* gameController, int x, int y, float height, float width) {
+	Player::body = addBodyPlayer(gameController, x, y, height, width);
 	Player::shape = std::unique_ptr<Circle>(new Circle());
 }
+
 
 void Player::draw(sf::Color color, sf::RenderWindow* window) {
 	Player::shape->draw(body, color, window);
--- unitTests/unitTests.cpp
+++ unitTests/unitTests.cpp
@@ -47,7 +47,7 @@ TEST(TestAddBody, TestAddPlatform) {
 	// Setting up the gameController
 	Box2DEngine gameController(WIDTH, HEIGHT);
 	b2World* world = gameController.getPhysicsWorld();
-	// adding one player
+	// adding one platform
 	Platform* platform = new Platform(&gameController, 300, 300, 10,100);
 	b2Body* platform_body = platform->getBody();
 	// Going through the box2D bodies list
@@ -64,7 +64,7 @@ TEST(TestAddBody, TestAddRope) {
 	// Setting up the gameController
 	Box2DEngine gameController(WIDTH, HEIGHT);
 	b2World* world = gameController.getPhysicsWorld();
-	// adding one player
+	// adding one rope
 	Rope* rope = new Rope(10 * RATIO, 30, &gameController);
 
 	std::vector<b2Body*> elements = rope->getElements();
@@ -75,4 +75,46 @@ TEST(TestAddBody, TestAddRope) {
 		it = std::find(elements.begin(), elements.end(), BodyIterator);
 		EXPECT_NE(it, elements.end());
 	}
+}
+
+// One player over a platform
+TEST(TestInteractionBodies, TestPlayerPlatform1) {
+	// Setting up the gameController
+	Box2DEngine gameController(WIDTH, HEIGHT);
+	b2World* world = gameController.getPhysicsWorld();	
+	sf::RenderWindow* window = gameController.getApp();
+
+	// adding one player
+	Player* player = new Player(&gameController, 320, 100);
+
+	// adding one platform
+	Platform* platform = new Platform(&gameController, 300, 300, 10, 100);
+
+	// Simulating over some iterations
+
+	// Simulation parameters
+	float timeStep = 1.0f / 60.0f;
+	int32 velocityIterations = 6;
+	int32 positionIterations = 2;
+
+	// 50 iterations to make the player fall (50*2 = 100 meters high)
+	for (int i = 0; i < 5000; i++) {
+		world->Step(timeStep, velocityIterations, positionIterations);
+		platform->draw(sf::Color::Green, window);
+		player->draw(sf::Color::Red, window);
+		window->display();
+	}
+	b2Vec2 oldPosition = player->getBody()->GetPosition();
+	// Another round of simulations to check that Player is stationary
+	for (int i = 0; i < 10; i++) {
+		world->Step(timeStep, velocityIterations, positionIterations);
+		b2Vec2 newPosition = player->getBody()->GetPosition();
+		platform->draw(sf::Color::Green, window);
+		player->draw(sf::Color::Red, window);
+		window->display();
+
+		//EXPECT_EQ(oldPosition.x, newPosition.x);
+		//EXPECT_EQ(oldPosition.y, newPosition.y);
+	}
+
 }
