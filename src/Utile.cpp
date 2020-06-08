431a6354edaf2fafc07511697164e3398947af69
Bastien Porte <bastien.porte@telecom-sudparis.eu>
2020-06-08 20:14:58 +0200

    Clean up Utile

--- src/Utile.cpp
+++ src/Utile.cpp
@@ -1,41 +1,5 @@
 #include "Utile.h"
 
-void getvalue(b2Body* player1, int* footcount, int* handcount, int* jumptimout) {
-	b2Fixture* playerfixtures = player1->GetFixtureList();
-	while (playerfixtures != nullptr) {
-		FixtureData* userdata = ((FixtureData*)playerfixtures->GetUserData());
-		if (userdata == nullptr)
-			return;
-		int datatype = userdata->getDataType();
-		switch (datatype)
-		{
-		case foot:
-			*footcount = ((FootData*)userdata)->GetNumFootContact();
-			break;
-		case hand:
-			*handcount = ((HandData*)userdata)->GetNumhandContact();
-			break;
-		case player:
-			*jumptimout = ((PlayerData*)userdata)->GetJumpTimeout();
-			break;
-		default:
-			printf("attention type de features non prise en compte ");
-			break;
-		}
-		playerfixtures = playerfixtures->GetNext();
-	}
-}
-
-PlayerData* getPlayerData(b2Body* player1) {
-	b2Fixture* playerfixtures = player1->GetFixtureList();
-	while (playerfixtures != nullptr) {
-		FixtureData* userdata = ((FixtureData*)playerfixtures->GetUserData());
-		if (userdata->getDataType() == player)
-			return (PlayerData*)userdata;
-		playerfixtures = playerfixtures->GetNext();
-	}
-}
-
 std::pair<float, float> getBodyDimensions(b2Body* body) { // only for squares right now 
 	b2Fixture* fixture = body->GetFixtureList();
 	float width, height;
