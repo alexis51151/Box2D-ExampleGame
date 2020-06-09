f5c238f1b6749de0e3dbe0112ffa2187640b43db
Bastien Porte <bastien.porte@telecom-sudparis.eu>
2020-06-09 14:21:37 +0200

    erreur du aux merge

--- src/playerrightcommand.h
+++ src/playerrightcommand.h
@@ -1,9 +1,7 @@
 #pragma once
-<<<<<<< HEAD
-#include "Icomand.h"
-=======
 
->>>>>>> fcf14ea275641dd9d13ad2600cb359de9f125d58
+#include "Icomand.h"
+
 class PlayerRighttcomand : public Icommand
 {
 public:
