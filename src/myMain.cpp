eb66beea3602992b2ae8c22ef1d432df24d2edd4
alexis51151 <44121643+alexis51151@users.noreply.github.com>
2020-06-09 12:04:25 +0200

    Ajout des commandes dans Event.cpp en fonction du fichier de config

--- src/myMain.cpp
+++ src/myMain.cpp
@@ -3,6 +3,10 @@
 
 int myMain() {
 
+	// Retrieve game config
+	XMLParser configParser("C:/Users/alexi/source/repos/Jeu_jin/resources/config.xml");
+	configParser.readConfig();
+
 
 	// Box2D world creation using Box2DEngine class form Box2DEngine.cpp
 	Box2DEngine gameController(WIDTH, HEIGHT);
@@ -36,7 +40,7 @@ int myMain() {
 	while (window->isOpen())
 	{
 		// Events.cpp : handle mouse and keyboard events
-		HookEvents(window, &gameController, &players, &platforms);
+		HookEvents(window, &gameController, &players, &platforms, &(configParser.getKeyboardCommandsPlayer1()), &(configParser.getKeyboardCommandsPlayer1()));
 		window->clear(sf::Color::White);
 
 		world->Step(timeStep, velocityIterations, positionIterations);
